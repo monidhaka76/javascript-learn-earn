e72e192563f9b1fa2cce60ec5991409f6fc3cfee
monidhaka76 <monidhaka76@gmail.com>
2025-08-19 20:32:42 +0600

    infinity

--- variable.js
+++ variable.js
@@ -4,11 +4,17 @@
 // console.log(apples);
 // console.log(apples + oranges);
 
-var first = 0.3;
-var second = 0.4;
+// var first = 0.3;
+// var second = 0.4;
 
-var total = first+second;
-console.log(total.toFixed(1));
+// var total = first + second;
+// console.log(total.toFixed(1));
+
+const number = 50/0;
+console.log(number);
+
+const number = -50/0;
+console.log(number);
 
 
 
