393d5c782646f107f325a170387fbf90bb64557d
monidhaka76 <monidhaka76@gmail.com>
2025-08-19 20:23:35 +0600

    tofixed

--- variable.js
+++ variable.js
@@ -1,8 +1,16 @@
-var apples = parseInt('400');
-var oranges = 600;
+// var apples = parseInt('400');
+// var oranges = 600;
 
-console.log(apples);
-console.log(apples + oranges);
+// console.log(apples);
+// console.log(apples + oranges);
+
+var first = 0.3;
+var second = 0.4;
+
+var total = first+second;
+console.log(total.toFixed(1));
+
+
 
 
 
